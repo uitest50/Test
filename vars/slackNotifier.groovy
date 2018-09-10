a75a555e47fce492739b6c4e80af0de90f48e688
Akash Gupta <akash.gupta@QUALITEST.LOCAL>
2018-09-10 19:25:09 +0530

    version 1.1.0

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,6 +1,6 @@
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "*Job*: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    slackSend color: "good",  message: "*Job*: ${env.JOB_NAME} \n *BuildNumber* : ${env.BUILD_NUMBER} \n *Result*: PASS \n *Jenkins Build URL*: ${env.BUILD_URL}console "
   }
   else if( buildResult == "FAILURE" ) { 
     slackSend color: "danger", message: "*Job*: ${env.JOB_NAME} \n *BuildNumber* : ${env.BUILD_NUMBER} \n *Result*: FAIL \n *Jenkins Build URL*: ${env.BUILD_URL}console  "
@@ -12,3 +12,7 @@ def call(String buildResult) {
     slackSend color: "danger", message: "*Job*: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its result was unclear"	
   }
 }
+
+slackNotifier(currentBuild.currentResult)
+
+library 'Jenkins-shared-library'

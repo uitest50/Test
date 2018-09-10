fa1755c8297d5197be231fb7ae66440af55a88cb
Akash Gupta <akash.gupta@QUALITEST.LOCAL>
2018-09-10 17:21:58 +0530

    v 1.1

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -3,7 +3,7 @@ def call(String buildResult) {
     slackSend color: "good", message: "*Job*: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "*Job*: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+    slackSend color: "danger", message: "*Job*: ${env.JOB_NAME}" ```"with buildnumber ${env.BUILD_NUMBER} was failed```"
   }
   else if( buildResult == "UNSTABLE" ) { 
     slackSend color: "warning", message: "*Job*: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
